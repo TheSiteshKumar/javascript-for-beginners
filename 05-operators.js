a3843f8f12e2d33a7fb2e027459b94a14cbf7fde
Sitesh Kumar <thesiteshkumar@gmail.com>
2025-07-10 18:16:32 +0530

    05 operator

---
+++ 05-operators.js
@@ -0,0 +1,62 @@
+// 1. Arithmetic Operators  ( +, -, *, /, %)
+// let a = 10;
+// let b = 3;
+
+// console.log("Arithmetic Operators:");
+// console.log("a + b =", a + b); // 13
+// console.log("a - b =", a - b); // 7
+// console.log("a * b =", a * b); // 30
+// console.log("a / b =", a / b); // 3.333...
+// console.log("a % b =", a % b); // 1
+
+// 2. Comparison Operators
+//  {==, ===, !=, !==, >, <, >=, <= }
+// (return - True or False)
+// let x = 5;
+// let y = "5";
+
+// console.log("Comparison Operators:");
+// console.log("x == y  ->", x == y);   // true (value same)
+// console.log("x === y ->", x === y);  // false (checking typeof and value same)
+// console.log("x != y  ->", x != y);   // false (value same)
+// console.log("x !== y ->", x !== y);  // true (type different)
+// console.log("x > 3   ->", x > 3);    // true
+// console.log("x < 10  ->", x < 10);   // true
+// console.log("x >= 5  ->", x >= 5);   // true
+// console.log("x <= 5  ->", x <= 5);   // true
+// console.log("x <= 5  ->", x < y);    // false
+
+// console.log(true == true)
+// let xyz = (3>2)
+// let abc = (2>1)
+// console.log ( false == false )
+// true == true
+
+// 3. Logical Operators (&&: AND , ||: OR, !: NOT)
+
+// let a = 10;
+// let b = 5;
+
+// let x = 15;
+// let y = 20;
+
+// console.log((a>b) && (x<y))
+// console.log((a>b) || (x<y))
+// console.log((a<b) || (y<x))
+// console.log(10 == 20)
+
+// 4. Assignment Operators
+// let num = 111;
+// num +=  10   // num = num +10
+// num -=  10   // num = num -10
+
+// console.log(num);
+
+// 5. Unary Operators
+// let count = 1;
+// let count2 = 1;
+// count++; // 2
+// console.log("count++ ->", count);
+
+// count2--; // 0
+// console.log("count2-- ->", count2);
